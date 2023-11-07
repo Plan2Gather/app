cf64796f48b4c7c514da1ead14c401ebe200278b
Chris <chris2fourlaw@gmail.com>
2023-11-06 17:56:50 -0800

    feat: add 404 page

---
+++ packages/frontend/src/app/pages/not-found/not-found.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta } from '@storybook/react';
+import NotFound from './not-found';
+
+const meta: Meta<typeof NotFound> = {
+  component: NotFound,
+  title: 'NotFound',
+};
+export default meta;
+
+export const Primary = {
+  args: {},
+};
