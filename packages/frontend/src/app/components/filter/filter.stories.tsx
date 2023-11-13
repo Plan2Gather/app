0c9181d3378031089f3f347375c89617ca3fb707
Naomi <97060752+nam0mi@users.noreply.github.com>
2023-11-13 15:57:25 -0800

    feat: add filter component (#51)
    * created a new filter feature
    
    * fixed formatting
    
    * fixed order of imports

---
+++ packages/frontend/src/app/components/filter/filter.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta } from '@storybook/react';
+import Filter from './filter';
+
+const meta: Meta<typeof Filter> = {
+  component: Filter,
+  title: 'Filter',
+};
+export default meta;
+
+export const Primary = {
+  args: {
+    data: {
+      availability: {
+        'user test 1': {},
+        'user test 2': {},
+      },
+    },
+  },
+};
