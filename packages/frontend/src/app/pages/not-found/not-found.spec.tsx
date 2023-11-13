b64745dc6be4dc5cc454fccf7ab964dd4919c9c9
Chris <chris2fourlaw@gmail.com>
2023-11-13 13:41:08 -0800

    feat: improve test comprehension and coverage

--- packages/frontend/src/app/pages/not-found/not-found.spec.tsx
+++ packages/frontend/src/app/pages/not-found/not-found.spec.tsx
@@ -1,4 +1,5 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 import NotFound from './not-found';
 
@@ -6,5 +7,12 @@ describe('NotFound', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<NotFound />);
     expect(baseElement).toBeTruthy();
+
+    expect(screen.queryByText('404 Not Found')).toBeInTheDocument();
+    expect(
+      screen.queryByText(
+        'The requested page could not be found. Please check the URL and try again.'
+      )
+    ).toBeInTheDocument();
   });
 });
