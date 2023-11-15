54c4bd181c55989b7296ef0d4b7ef59013d0bef7
Chris <chris2fourlaw@gmail.com>
2023-11-15 13:59:54 -0800

    fix: tests & test data

--- packages/frontend/src/app/components/gathering-creation-stepper/confirmation/confirmation.spec.tsx
+++ packages/frontend/src/app/components/gathering-creation-stepper/confirmation/confirmation.spec.tsx
@@ -1,10 +1,25 @@
 import { render } from '@testing-library/react';
 
+import { GatheringFormData } from '@plan2gather/backend/types';
 import Confirmation from './confirmation';
+
+const defaultGatheringData: GatheringFormData = {
+  name: 'Test Gathering',
+  description: 'Test Gathering Description',
+  timezone: 'America/New_York',
+  allowedPeriods: [
+    {
+      start: '2021-10-10T12:00:00.000Z',
+      end: '2021-10-10T13:00:00.000Z',
+    },
+  ],
+};
 
 describe('Confirmation', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Confirmation />);
+    const { baseElement } = render(
+      <Confirmation gatheringData={defaultGatheringData} />
+    );
     expect(baseElement).toBeTruthy();
   });
 });
--- packages/frontend/src/app/components/gathering-details/gathering-details.spec.tsx
+++ packages/frontend/src/app/components/gathering-details/gathering-details.spec.tsx
@@ -1,13 +1,18 @@
 import { render } from '@testing-library/react';
 
+import { GatheringFormData } from '@plan2gather/backend/types';
 import { GatheringDetails } from './gathering-details';
 
-const defaultGatheringData = {
+const defaultGatheringData: GatheringFormData = {
   name: 'Test Gathering',
   description: 'Test Gathering Description',
   timezone: 'America/New_York',
-  start_time: '2021-10-10T12:00:00.000Z',
-  end_time: '2021-10-10T13:00:00.000Z',
+  allowedPeriods: [
+    {
+      start: '2021-10-10T12:00:00.000Z',
+      end: '2021-10-10T13:00:00.000Z',
+    },
+  ],
 };
 
 describe('GatheringDetails', () => {
