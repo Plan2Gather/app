ecd30d37b9d7823f171f8b3a136736817ba1c029
nam0mi <97060752+nam0mi@users.noreply.github.com>
2023-11-15 13:44:30 -0800

    created a sample about us page, need to fix image rendering

--- packages/frontend/src/app/pages/team/team.tsx
+++ packages/frontend/src/app/pages/team/team.tsx
@@ -1,9 +1,64 @@
+import React from 'react';
+import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
 
-export default function NotFound() {
+export default function Team() {
+  const teamMembers = [
+    {
+      id: 1,
+      name: 'Chris Lawson',
+      major: '4th Year Computer Science',
+      image: 'chris.png', // Add the path to your image, take github profile pics
+    },
+    {
+      id: 2,
+      name: 'Naomi Nayman',
+      major: '3rd Year Computer Science',
+      image: 'naomi.png',
+    },
+    {
+      id: 3,
+      name: 'Sam Bock',
+      major: '3rd Year Computer Science',
+      image: 'sam.png',
+    },
+    {
+      id: 4,
+      name: 'Spencer Perley',
+      major: '3rd Year Computer Science',
+      image: 'spencer.png',
+    },
+  ];
+
   return (
-    <Typography component="h1" variant="h3">
-      Team
-    </Typography>
+    <Container>
+      <Typography variant="h2" align="center" gutterBottom>
+        About Us
+      </Typography>
+      <Grid container spacing={3}>
+        {teamMembers.map((member) => (
+          <Grid item xs={12} sm={6} md={4} key={member.id}>
+            <Avatar
+              alt={member.name}
+              src={member.image}
+              sx={{ width: 150, height: 150, margin: 'auto' }}
+            />
+            <Typography variant="h5" align="center" gutterBottom>
+              {member.name}
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              {member.major}
+            </Typography>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 }
