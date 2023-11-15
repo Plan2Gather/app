d585392a7103428cb2284d2276fe1cbda92c482f
Chris <chris2fourlaw@gmail.com>
2023-11-15 14:33:57 -0800

    feat: add basic view page

---
+++ packages/frontend/src/app/pages/gathering-view/gathering-view.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta } from '@storybook/react';
+import GatheringView from './gathering-view';
+
+const meta: Meta<typeof GatheringView> = {
+  component: GatheringView,
+  title: 'GatheringView',
+};
+export default meta;
+
+export const Primary = {
+  args: {},
+};
