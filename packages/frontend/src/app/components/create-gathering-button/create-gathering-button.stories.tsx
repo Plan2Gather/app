0b0b0c136d79285d52e1339fbac9b626065010fa
cjlawson02 <7269141+cjlawson02@users.noreply.github.com>
2023-11-15 10:57:36 -0800

    feat: fix storybook theme rendering (#52)
    * feat: fix storybook theme rendering
    
    * fix: format
    
    * fix: downgrade prettier

--- packages/frontend/src/app/components/create-gathering-button/create-gathering-button.stories.tsx
+++ packages/frontend/src/app/components/create-gathering-button/create-gathering-button.stories.tsx
@@ -4,6 +4,12 @@ import CreateGatheringButton from './create-gathering-button';
 const meta: Meta<typeof CreateGatheringButton> = {
   component: CreateGatheringButton,
   title: 'CreateGatheringButton',
+  argTypes: {
+    variant: {
+      options: ['toolbar', 'homepage'],
+      control: { type: 'radio' },
+    },
+  },
 };
 export default meta;
 
