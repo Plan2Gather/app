f57c15db57ed1e47098b4896310495f1c6ff588b
Chris <chris2fourlaw@gmail.com>
2023-12-01 13:02:09 -0800

    fix: improve team styling

--- packages/frontend/src/app/pages/team/team.tsx
+++ packages/frontend/src/app/pages/team/team.tsx
@@ -1,50 +1,44 @@
-import React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
+import Grid from '@mui/material/Unstable_Grid2';
 import Container from '@mui/material/Container';
 
 export default function Team() {
   const teamMembers = [
     {
-      id: 1,
       name: 'Chris Lawson',
       major: '4th Year Computer Science',
-      image: 'https://avatars.githubusercontent.com/u/7269141?s=60&v=4', // Add the path to your image, take github profile pics
+      image: 'https://avatars.githubusercontent.com/u/7269141', // Add the path to your image, take github profile pics
       bio: 'Insert a bio for yourself in packages/frontend/src/app/pages/team/team.tsx',
     },
     {
-      id: 2,
       name: 'Naomi Nayman',
       major: '3rd Year Computer Science',
-      image: 'https://avatars.githubusercontent.com/u/97060752?v=4',
+      image: 'https://avatars.githubusercontent.com/u/97060752',
       bio: 'Insert a bio for yourself in packages/frontend/src/app/pages/team/team.tsx',
     },
     {
-      id: 3,
       name: 'Sam Bock',
       major: '3rd Year Computer Science',
-      image: 'https://avatars.githubusercontent.com/u/63836618?s=40&v=4',
+      image: 'https://avatars.githubusercontent.com/u/63836618',
       bio: 'Insert a bio for yourself in packages/frontend/src/app/pages/team/team.tsx',
     },
     {
-      id: 4,
       name: 'Spencer Perley',
       major: '3rd Year Computer Science',
-      image:
-        'https://avatars.githubusercontent.com/u/63747892?s=40&u=cee118b809262c9533aa708b57437f0eb257c818&v=4',
+      image: 'https://avatars.githubusercontent.com/u/63747892',
       bio: 'Insert a bio for yourself in packages/frontend/src/app/pages/team/team.tsx',
     },
   ];
 
   return (
     <Container>
-      <Typography variant="h2" align="center" gutterBottom>
-        About Us
+      <Typography component="h1" variant="h3" align="center" gutterBottom>
+        Team
       </Typography>
-      <Grid container spacing={3}>
+      <Grid container spacing={3} justifyContent="center">
         {teamMembers.map((member) => (
-          <Grid item xs={12} sm={6} md={4} key={member.id}>
+          <Grid xs={12} sm={6} md={4} key={member.name}>
             <Avatar
               alt={member.name}
               src={member.image}
