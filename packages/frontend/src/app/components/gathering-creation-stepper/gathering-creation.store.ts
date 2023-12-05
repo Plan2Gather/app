131b2338977cc95e29eafba0d321537b88e10db4
cjlawson02 <7269141+cjlawson02@users.noreply.github.com>
2023-12-05 14:39:44 -0800

    feat: refactor form state management (#67)

--- packages/frontend/src/app/components/gathering-creation-stepper/gathering-creation.store.ts
+++ packages/frontend/src/app/components/gathering-creation-stepper/gathering-creation.store.ts
@@ -1,85 +1,24 @@
 import { create } from 'zustand';
 import { GatheringFormDetails, Weekday } from '@plan2gather/backend/types';
 import { DateTime } from 'luxon';
-import { DateRangeLuxon } from '../time-range-selections/time-range-picker/time-range-picker';
 
 export interface GatheringStepperFormData {
   details: GatheringFormDetails | null;
   possibleDates: Weekday[];
-  timePeriods: Partial<Record<Weekday, DateRangeLuxon[]>>;
-  timePeriodsFormData: Partial<
-    Record<Weekday, Record<string, DateTime>>
-  > | null;
+  timePeriods: Record<string, DateTime>;
   setDetails: (details: GatheringFormDetails) => void;
   setPossibleDates: (possibleDates: Weekday[]) => void;
-  setTimePeriods: (
-    timePeriods: Partial<Record<Weekday, DateRangeLuxon[]>>
-  ) => void;
-  setTimePeriodsForDay: (day: Weekday, timePeriods: DateRangeLuxon[]) => void;
-  addTimePeriodForDay: (day: Weekday, timePeriod: DateRangeLuxon) => void;
-  removeTimePeriodForDay: (day: Weekday, id: string) => void;
-  updateTimePeriodForDay: (day: Weekday, timePeriods: DateRangeLuxon) => void;
-  setTimePeriodsFormData: (
-    day: Weekday,
-    data: Record<string, DateTime>
-  ) => void;
+  setTimePeriods: (timePeriods: Record<string, DateTime>) => void;
 }
 
 const useGatheringStepperFormData = create<GatheringStepperFormData>((set) => ({
   details: null,
   possibleDates: [],
-  timePeriods: {} as Record<Weekday, DateRangeLuxon[]>,
+  timePeriods: {},
   timePeriodsFormData: null,
   setDetails: (details) => set({ details }),
   setPossibleDates: (possibleDates) => set({ possibleDates }),
   setTimePeriods: (timePeriods) => set({ timePeriods }),
-  setTimePeriodsForDay: (day, timePeriods) =>
-    set((state) => ({
-      timePeriods: {
-        ...state.timePeriods,
-        [day]: timePeriods,
-      },
-    })),
-  addTimePeriodForDay: (day, timePeriod) =>
-    set((state) => {
-      const existing = state.timePeriods[day];
-
-      let periods: DateRangeLuxon[] = [];
-      if (existing) {
-        periods = [...existing];
-      }
-
-      return {
-        timePeriods: {
-          ...state.timePeriods,
-          [day]: [...periods, timePeriod],
-        },
-      };
-    }),
-  removeTimePeriodForDay: (day, id) =>
-    set((state) => ({
-      timePeriods: {
-        ...state.timePeriods,
-        [day]: state.timePeriods[day]?.filter((tp) => tp.id !== id),
-      },
-    })),
-  updateTimePeriodForDay: (day, timePeriod) =>
-    set((state) => ({
-      timePeriods: {
-        ...state.timePeriods,
-        [day]: state.timePeriods[day]?.map((tp) =>
-          tp.id === timePeriod.id ? timePeriod : tp
-        ),
-      },
-    })),
-  setTimePeriodsFormData: (day, data) =>
-    set((state) => ({
-      ...state,
-      timePeriodsFormData: {
-        ...state.timePeriodsFormData,
-        [day]: data, // Ensuring data is always of type Record<string, DateTime>
-      },
-    })),
 }));
 
 export default useGatheringStepperFormData;
--- packages/frontend/src/app/components/time-range-selections/time-range-selections.tsx
+++ packages/frontend/src/app/components/time-range-selections/time-range-selections.tsx
@@ -1,112 +1,51 @@
-import {
-  forwardRef,
-  useCallback,
-  useEffect,
-  useImperativeHandle,
-  useState,
-} from 'react';
+import { useCallback, useState } from 'react';
 import IconButton from '@mui/material/IconButton';
 import AddCircleIcon from '@mui/icons-material/AddCircleOutline';
 import Stack from '@mui/material/Stack';
-import { useForm, FormContainer } from 'react-hook-form-mui';
 import { v4 as uuidv4 } from 'uuid';
-import { DateTime } from 'luxon';
 import { Weekday } from '@plan2gather/backend/types';
-import TimeRangePicker, {
-  DateRangeLuxon,
-} from './time-range-picker/time-range-picker';
+import TimeRangePicker from './time-range-picker/time-range-picker';
 import useGatheringStepperFormData from '../gathering-creation-stepper/gathering-creation.store';
 
 type TimeRangeSelectionsProps = {
   day: Weekday;
 };
 
-const TimeRangeSelections = forwardRef<unknown, TimeRangeSelectionsProps>(
-  ({ day }, ref) => {
-    const store = useGatheringStepperFormData();
+function TimeRangeSelections({ day }: TimeRangeSelectionsProps) {
+  const store = useGatheringStepperFormData();
 
-    const [timePeriods, setTimePeriods] = useState<DateRangeLuxon[]>(
-      store.timePeriods[day] ?? []
-    );
+  const [timePeriods, setTimePeriods] = useState<{ id: string }[]>([]);
 
-    const addTimePeriod = useCallback(() => {
-      setTimePeriods((prev) => [
-        ...prev,
-        {
-          start: null,
-          end: null,
-          id: uuidv4(),
-        },
-      ]);
-    }, []);
-
-    const removeTimePeriod = useCallback((id: string) => {
-      setTimePeriods((prev) => prev.filter((range) => range.id !== id));
-    }, []);
-
-    const formContext = useForm<Record<string, DateTime>>({
-      defaultValues: store.timePeriodsFormData?.[day] ?? {},
-    });
-
-    useEffect(() => {
-      const values = formContext.watch();
-
-      setTimePeriods((prevTimePeriods) =>
-        prevTimePeriods.map((range, index) => {
-          // Construct keys based on current index
-          const startKey = `${day}_${index}_start`;
-          const endKey = `${day}_${index}_end`;
-
-          // New range object
-          const newRange = { ...range };
-          if (values[startKey]) {
-            newRange.start = values[startKey];
-          }
-          if (values[endKey]) {
-            newRange.end = values[endKey];
-          }
-
-          return newRange;
-        })
-      );
-    }, [formContext, day]);
-
-    useImperativeHandle(ref, () => ({
-      submit: async () => {
-        const isFormValid = await new Promise((resolve) => {
-          formContext.handleSubmit(
-            (data) => {
-              store.setTimePeriodsForDay(day, timePeriods);
-              store.setTimePeriodsFormData(day, data);
-              resolve(true);
-            },
-            () => resolve(false)
-          )();
-        });
-
-        return isFormValid;
+  const addTimePeriod = useCallback(() => {
+    setTimePeriods((prev) => [
+      ...prev,
+      {
+        id: uuidv4(),
       },
-    }));
+    ]);
+  }, []);
 
-    return (
-      <FormContainer formContext={formContext}>
-        <Stack spacing={1}>
-          {timePeriods.map((range, index) => (
-            <TimeRangePicker
-              key={range.id}
-              namePrefix={`${day}_${index}`}
-              range={range}
-              timezone={store.details!.timezone}
-              onRemove={() => removeTimePeriod(range.id)}
-            />
-          ))}
-        </Stack>
-        <IconButton size="large" onClick={() => addTimePeriod()}>
-          <AddCircleIcon fontSize="inherit" />
-        </IconButton>
-      </FormContainer>
-    );
-  }
-);
+  const removeTimePeriod = useCallback((id: string) => {
+    setTimePeriods((prev) => prev.filter((range) => range.id !== id));
+  }, []);
+
+  return (
+    <>
+      <Stack spacing={1}>
+        {timePeriods.map((range, index) => (
+          <TimeRangePicker
+            key={range.id}
+            namePrefix={`${day}_${index}`}
+            timezone={store.details!.timezone}
+            onRemove={() => removeTimePeriod(range.id)}
+          />
+        ))}
+      </Stack>
+      <IconButton size="large" onClick={() => addTimePeriod()}>
+        <AddCircleIcon fontSize="inherit" />
+      </IconButton>
+    </>
+  );
+}
 
 export default TimeRangeSelections;
