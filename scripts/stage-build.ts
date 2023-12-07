371a9fe22353cc25ff36ecd3568d550e81854a7f
Chris <chris2fourlaw@gmail.com>
2023-12-07 09:58:00 -0800

    fix: use CF_PAGES_BRANCH var

--- scripts/stage-build.ts
+++ scripts/stage-build.ts
@@ -1,22 +1,22 @@
 import { execSync } from 'child_process';
 
-let mode = process.env.NX_BUILD_MODE; // Environment variable
+let branch = process.env.CF_PAGES_BRANCH; // Environment variable
 
-// Valid modes are 'dev', 'beta', and 'production'
-if (!mode || !['dev', 'beta', 'production'].includes(mode)) {
-  throw new Error(`Invalid mode: ${mode}`);
-}
+console.log(`Building for branch: ${branch}`);
 
-if (mode === 'beta') {
-  // Check if the branch is 'beta'. If not, change to dev
-  const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
-  if (branch !== 'beta') {
-    console.log(`Not on beta branch, using dev mode for branch ${branch}`);
+let mode = 'dev';
+switch (branch) {
+  case 'main':
+    mode = 'prod';
+    break;
+  case 'beta':
+    mode = 'beta';
+    break;
+  default:
     mode = 'dev';
-  }
 }
 
-console.log(`Building frontend in ${mode} mode`);
+console.log(`Building with mode: ${mode}`);
 
 // Run the NX build command with the mode
 execSync(`nx run frontend:build:${mode}`, { stdio: 'inherit' });
