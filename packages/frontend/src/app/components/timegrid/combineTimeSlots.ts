72660535fcfc40668257d1aefdcbd0ac2ba95780
spencerperley <63747892+spencerperley@users.noreply.github.com>
2023-12-07 15:37:42 -0800

    Add TIme grid Components
    also added parsing stuff

---
+++ packages/frontend/src/app/components/timegrid/combineTimeSlots.ts
@@ -0,0 +1,161 @@
+// ToDo: add tests but will push first becase I think we may change the inputs and output formats
+//import type { UserAvailability, Weekday } from '@plan2gather/backend/types';
+/*
+takes: a list of a list of time periods
+
+returns a list of time periods as well as 
+*/
+
+// ToDo This is a bad approach if it is too slow i will remake it with a sliding window aproach!
+function combineTimeSlots(
+    groupTimePeriods: {
+        name: string;
+        availability: Record<string, { start: string; end: string }[]>;
+    }[]
+): Record<string, {  start: string; end: string; names: string[] }[]> {
+    let finalResult: Record<
+        string,
+        {  start: string; end: string; names: string[] }[]
+    > = {};
+    let usedDays: Set<string> = new Set();
+
+    for (const person of groupTimePeriods) {
+        for (const dayOfWeek in person.availability) {
+            usedDays.add(dayOfWeek);
+        }
+    }
+
+    
+
+    for (const day of usedDays) {
+        finalResult[day] = [];
+        let dayStartStopSet: Set<string> = new Set();
+        
+        for (const person of groupTimePeriods) {
+            let dayAvalability: {  start: string; end: string }[] =
+                person.availability[day] ?? [];
+            
+            
+            for (const period of dayAvalability) {
+                dayStartStopSet.add(period.start);
+                dayStartStopSet.add(period.end);
+            }
+        }
+
+        let dayStartStop = createStartStopFromSeries(
+            Array.from(dayStartStopSet).sort()
+        );
+
+        
+
+        for (const period of dayStartStop) {
+            let tempPeriod: { start: string; end: string; names: string[] } = {
+                start: period.start,
+                end: period.end,
+                names: [],
+            };
+            for (const person of groupTimePeriods) {
+                if (
+                    isAvalable(
+                        person.availability[day],
+                        period.start,
+                        period.end
+                    )
+                ) {
+                    tempPeriod.names.push(person.name);
+                }
+            }
+            finalResult[day].push(tempPeriod);
+        }
+    }
+
+    return finalResult;
+}
+
+//ToDo: make this use binary search in stead of just iterating throught
+function isAvalable(
+    individualTimePeriods: {  start: string; end: string}[],
+    start: string,
+    end: string
+): boolean {
+    for (const timePeriod of individualTimePeriods) {
+        if (Date.parse(start) >= Date.parse(timePeriod.start) && Date.parse(end) <= Date.parse(timePeriod.end)) {
+            return true;
+        }
+        if (Date.parse(timePeriod.start) >= Date.parse(end)) {
+            break;
+        }
+    }
+    return false;
+}
+
+function createStartStopFromSeries(
+    values: string[]
+): {  start: string; end: string }[] {
+    const result: {  start: string; end: string }[] = [];
+
+    for (let i = 0; i < values.length - 1; i++) {
+        result.push({
+            start: values[i],
+            end: values[i + 1],
+        });
+    }
+
+    return result;
+}
+/* //simple test non extensive
+let test: [number,number][][]= [[[0,3],[6,8]],
+                            [[1,4],[5,7]],
+                            [[1,2],[3,4],[8,10]]];
+
+console.log(combineTimeSlots(test));
+*/
+
+const test = [
+    {
+        name: 'Chris',
+        availability: {
+            monday: [
+                {
+                    start: '2023-12-06T09:00:00.000-08:00',
+                    end: '2023-12-06T17:00:00.000-08:00',
+                },
+                {
+                    start: '2023-12-06T09:00:00.000-08:00',
+                    end: '2023-12-06T09:30:00.000-08:00',
+                },
+            ],
+            friday: [
+                {
+                    start: '2023-12-06T09:00:00.000-08:00',
+                    end: '2023-12-06T09:30:00.000-08:00',
+                },
+            ],
+        },
+    },
+    {
+        name: 'Spencer',
+        availability: {
+            monday: [
+                {
+                    start: '2023-12-06T09:00:00.000-08:00',
+                    end: '2023-12-06T17:00:00.000-08:00',
+                },
+                {
+                    start: '2023-12-06T09:00:00.000-08:00',
+                    end: '2023-12-06T09:30:00.000-08:00',
+                },
+            ],
+            friday: [
+                {
+                    start: '2023-12-06T09:00:00.000-08:00',
+                    end: '2023-12-06T09:30:00.000-08:00',
+                },
+            ],
+        },
+    },
+];
+
+
+
+console.log(combineTimeSlots(test));
---
+++ packages/frontend/src/app/components/timegrid/timegrid.tsx
@@ -0,0 +1,102 @@
+import React, { FC, useState } from 'react';
+
+interface TimeGridProps {
+    data: {color :string, names : string[],period : {start :string,end : string}}[][];
+    columnLabels: string[];
+    rowLabels: string[];
+}
+
+const cellWidth = 100;
+const cellHeight = 10;
+
+const TimeGrid: FC<TimeGridProps> = ({ data, columnLabels, rowLabels }) => {
+    const [clickedCell, setClickedCell] = useState<{
+        rowIndex: number;
+        colIndex: number;
+    } | null>(null);
+
+    const handleCellClick = (rowIndex: number, colIndex: number) => {
+        setClickedCell({ rowIndex, colIndex });
+    };
+
+    return (
+        <div
+            style={{
+                position: 'relative',
+                overflow: 'auto',
+                display: 'grid',
+                gridTemplateColumns: `repeat(${
+                    data[0].length + 1
+                }, ${cellWidth}px)`,
+                border: 'none',
+            }}
+        >
+            {/* Empty cell for the top-left corner */}
+            <div
+                style={{
+                    width: '30px',
+                    height: '20px',
+                    backgroundColor: 'rgba(0, 0, 0, 0.0)',
+                    textAlign: 'center',
+                    lineHeight: '20px',
+                }}
+            ></div>
+
+            {/* Render column labels */}
+            {columnLabels.map((label, colIndex) => (
+                <div
+                    key={`col-label-${colIndex}`}
+                    style={{
+                        width: cellWidth,
+                        height: '20px',
+                        backgroundColor: 'rgba(0, 0, 0, 0.0)',
+                        textAlign: 'center',
+                        lineHeight: '20px',
+                    }}
+                >
+                    {label}
+                </div>
+            ))}
+
+            {/* Render data rows with row labels */}
+            {data.map((rowData, rowIndex) => (
+                // Render cells for each row
+                <>
+                    {/* Render row label */}
+                    <div
+                        key={`row-label-${rowIndex}`}
+                        style={{
+                            width: '30px',
+                            height: cellHeight,
+                            backgroundColor: 'rgba(0, 0, 0, 0.0)',
+                            textAlign: 'center',
+                            lineHeight: '10px',
+                        }}
+                    >
+                        {rowLabels[rowIndex]}
+                    </div>
+
+                    {/* Render data cells */}
+                    {rowData.map((cellData, colIndex) => (
+                        <div
+                            key={`${rowIndex}-${colIndex}`}
+                            style={{
+                                position: 'relative',
+                                width: cellWidth,
+                                height: cellHeight,
+                                backgroundColor: cellData.color,
+                                borderTop: '1x dotted black',
+                                borderBottom: '1px dotted black',
+                                borderLeft: '1px solid black',
+                                borderRight: '1px solid black',
+                            }}
+                            onClick={() => handleCellClick(rowIndex, colIndex)}
+                        ></div>
+                    ))}
+                </>
+            ))}
+        </div>
+    );
+};
+
+export default TimeGrid;
