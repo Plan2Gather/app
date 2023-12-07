404a2a02bc07c9b61a393c9eaa16373d1a01455e
spencerperley <63747892+spencerperley@users.noreply.github.com>
2023-12-07 15:38:38 -0800

    Formatted

--- packages/frontend/src/app/components/timegrid/combineTimeSlots.ts
+++ packages/frontend/src/app/components/timegrid/combineTimeSlots.ts
@@ -8,100 +8,92 @@ returns a list of time periods as well as
 
 // ToDo This is a bad approach if it is too slow i will remake it with a sliding window aproach!
 function combineTimeSlots(
-    groupTimePeriods: {
-        name: string;
-        availability: Record<string, { start: string; end: string }[]>;
-    }[]
-): Record<string, {  start: string; end: string; names: string[] }[]> {
-    let finalResult: Record<
-        string,
-        {  start: string; end: string; names: string[] }[]
-    > = {};
-    let usedDays: Set<string> = new Set();
+  groupTimePeriods: {
+    name: string;
+    availability: Record<string, { start: string; end: string }[]>;
+  }[]
+): Record<string, { start: string; end: string; names: string[] }[]> {
+  let finalResult: Record<
+    string,
+    { start: string; end: string; names: string[] }[]
+  > = {};
+  let usedDays: Set<string> = new Set();
+
+  for (const person of groupTimePeriods) {
+    for (const dayOfWeek in person.availability) {
+      usedDays.add(dayOfWeek);
+    }
+  }
+
+  for (const day of usedDays) {
+    finalResult[day] = [];
+    let dayStartStopSet: Set<string> = new Set();
 
     for (const person of groupTimePeriods) {
-        for (const dayOfWeek in person.availability) {
-            usedDays.add(dayOfWeek);
-        }
+      let dayAvalability: { start: string; end: string }[] =
+        person.availability[day] ?? [];
+
+      for (const period of dayAvalability) {
+        dayStartStopSet.add(period.start);
+        dayStartStopSet.add(period.end);
+      }
     }
 
-    
+    let dayStartStop = createStartStopFromSeries(
+      Array.from(dayStartStopSet).sort()
+    );
 
-    for (const day of usedDays) {
-        finalResult[day] = [];
-        let dayStartStopSet: Set<string> = new Set();
-        
-        for (const person of groupTimePeriods) {
-            let dayAvalability: {  start: string; end: string }[] =
-                person.availability[day] ?? [];
-            
-            
-            for (const period of dayAvalability) {
-                dayStartStopSet.add(period.start);
-                dayStartStopSet.add(period.end);
-            }
+    for (const period of dayStartStop) {
+      let tempPeriod: { start: string; end: string; names: string[] } = {
+        start: period.start,
+        end: period.end,
+        names: [],
+      };
+      for (const person of groupTimePeriods) {
+        if (isAvalable(person.availability[day], period.start, period.end)) {
+          tempPeriod.names.push(person.name);
         }
-
-        let dayStartStop = createStartStopFromSeries(
-            Array.from(dayStartStopSet).sort()
-        );
-
-        
-
-        for (const period of dayStartStop) {
-            let tempPeriod: { start: string; end: string; names: string[] } = {
-                start: period.start,
-                end: period.end,
-                names: [],
-            };
-            for (const person of groupTimePeriods) {
-                if (
-                    isAvalable(
-                        person.availability[day],
-                        period.start,
-                        period.end
-                    )
-                ) {
-                    tempPeriod.names.push(person.name);
-                }
-            }
-            finalResult[day].push(tempPeriod);
-        }
+      }
+      finalResult[day].push(tempPeriod);
     }
+  }
 
-    return finalResult;
+  return finalResult;
 }
 
 //ToDo: make this use binary search in stead of just iterating throught
 function isAvalable(
-    individualTimePeriods: {  start: string; end: string}[],
-    start: string,
-    end: string
+  individualTimePeriods: { start: string; end: string }[],
+  start: string,
+  end: string
 ): boolean {
-    for (const timePeriod of individualTimePeriods) {
-        if (Date.parse(start) >= Date.parse(timePeriod.start) && Date.parse(end) <= Date.parse(timePeriod.end)) {
-            return true;
-        }
-        if (Date.parse(timePeriod.start) >= Date.parse(end)) {
-            break;
-        }
+  for (const timePeriod of individualTimePeriods) {
+    if (
+      Date.parse(start) >= Date.parse(timePeriod.start) &&
+      Date.parse(end) <= Date.parse(timePeriod.end)
+    ) {
+      return true;
     }
-    return false;
+    if (Date.parse(timePeriod.start) >= Date.parse(end)) {
+      break;
+    }
+  }
+  return false;
 }
 
 function createStartStopFromSeries(
-    values: string[]
-): {  start: string; end: string }[] {
-    const result: {  start: string; end: string }[] = [];
+  values: string[]
+): { start: string; end: string }[] {
+  const result: { start: string; end: string }[] = [];
 
-    for (let i = 0; i < values.length - 1; i++) {
-        result.push({
-            start: values[i],
-            end: values[i + 1],
-        });
-    }
+  for (let i = 0; i < values.length - 1; i++) {
+    result.push({
+      start: values[i],
+      end: values[i + 1],
+    });
+  }
 
-    return result;
+  return result;
 }
 /* //simple test non extensive
 let test: [number,number][][]= [[[0,3],[6,8]],
@@ -112,50 +104,48 @@ console.log(combineTimeSlots(test));
 */
 
 const test = [
-    {
-        name: 'Chris',
-        availability: {
-            monday: [
-                {
-                    start: '2023-12-06T09:00:00.000-08:00',
-                    end: '2023-12-06T17:00:00.000-08:00',
-                },
-                {
-                    start: '2023-12-06T09:00:00.000-08:00',
-                    end: '2023-12-06T09:30:00.000-08:00',
-                },
-            ],
-            friday: [
-                {
-                    start: '2023-12-06T09:00:00.000-08:00',
-                    end: '2023-12-06T09:30:00.000-08:00',
-                },
-            ],
+  {
+    name: 'Chris',
+    availability: {
+      monday: [
+        {
+          start: '2023-12-06T09:00:00.000-08:00',
+          end: '2023-12-06T17:00:00.000-08:00',
         },
-    },
-    {
-        name: 'Spencer',
-        availability: {
-            monday: [
-                {
-                    start: '2023-12-06T09:00:00.000-08:00',
-                    end: '2023-12-06T17:00:00.000-08:00',
-                },
-                {
-                    start: '2023-12-06T09:00:00.000-08:00',
-                    end: '2023-12-06T09:30:00.000-08:00',
-                },
-            ],
-            friday: [
-                {
-                    start: '2023-12-06T09:00:00.000-08:00',
-                    end: '2023-12-06T09:30:00.000-08:00',
-                },
-            ],
+        {
+          start: '2023-12-06T09:00:00.000-08:00',
+          end: '2023-12-06T09:30:00.000-08:00',
         },
+      ],
+      friday: [
+        {
+          start: '2023-12-06T09:00:00.000-08:00',
+          end: '2023-12-06T09:30:00.000-08:00',
+        },
+      ],
     },
+  },
+  {
+    name: 'Spencer',
+    availability: {
+      monday: [
+        {
+          start: '2023-12-06T09:00:00.000-08:00',
+          end: '2023-12-06T17:00:00.000-08:00',
+        },
+        {
+          start: '2023-12-06T09:00:00.000-08:00',
+          end: '2023-12-06T09:30:00.000-08:00',
+        },
+      ],
+      friday: [
+        {
+          start: '2023-12-06T09:00:00.000-08:00',
+          end: '2023-12-06T09:30:00.000-08:00',
+        },
+      ],
+    },
+  },
 ];
-
-
 
 console.log(combineTimeSlots(test));
