ce405f6aed974b8b5cb19b4d3d366d90459597df
Chris <chris2fourlaw@gmail.com>
2023-12-07 09:50:22 -0800

    fix: check branch to help with stage

--- scripts/stage-build.ts
+++ scripts/stage-build.ts
@@ -1,10 +1,18 @@
 import { execSync } from 'child_process';
 
-const mode = process.env.NX_BUILD_MODE; // Environment variable
+let mode = process.env.NX_BUILD_MODE; // Environment variable
 
 // Valid modes are 'dev', 'beta', and 'production'
 if (!mode || !['dev', 'beta', 'production'].includes(mode)) {
   throw new Error(`Invalid mode: ${mode}`);
+}
+
+if (mode === 'beta') {
+  // Check if the branch is 'beta'. If not, change to dev
+  const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+  if (branch !== 'beta') {
+    mode = 'dev';
+  }
 }
 
 // Run the NX build command with the mode
