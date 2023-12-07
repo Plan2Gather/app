3e42159c364e831bd4323b22fa631d3c65741a9c
Chris <chris2fourlaw@gmail.com>
2023-12-07 11:59:24 -0800

    Revert "fix: test"
    This reverts commit d8970ebc94a400af2e04d6cbffe7d863ff04cd34.

--- packages/frontend/src/app/components/gathering-form/time-periods/time-range-selections/time-range-selections.spec.tsx
+++ packages/frontend/src/app/components/gathering-form/time-periods/time-range-selections/time-range-selections.spec.tsx
@@ -1,20 +1,17 @@
 import { render } from '@testing-library/react';
-import { FormContainer } from 'react-hook-form-mui';
 
 import TimeRangeSelections from './time-range-selections';
 
 describe('TimeRangeSelections', () => {
   it('should render successfully', () => {
     const { baseElement } = render(
-      <FormContainer>
-        <TimeRangeSelections
-          initial={{}}
-          day="wednesday"
-          allowMultiple={false}
-          restriction={undefined}
-          timezone="America/New_York"
-        />
-      </FormContainer>
+      <TimeRangeSelections
+        initial={{}}
+        day="wednesday"
+        allowMultiple={false}
+        restriction={undefined}
+        timezone="America/New_York"
+      />
     );
     expect(baseElement).toBeTruthy();
   });
