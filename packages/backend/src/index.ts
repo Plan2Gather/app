24da371483fe2ae36888d7699bea63dff6b46449
cjlawson02 <7269141+cjlawson02@users.noreply.github.com>
2023-12-07 10:13:18 -0800

    feat: CORS headers and fix backend stages (#76)
    * feat: CORS headers and fix backend stages
    
    * fix: check branch to help with stage
    
    * feat: add debug logs
    
    * fix: use CF_PAGES_BRANCH var

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -8,17 +8,47 @@ export const appRouter = t.router({
 });
 export type AppRouter = typeof appRouter;
 
-const CORS_HEADERS = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': '*',
-  'Access-Control-Allow-Headers': '*',
-};
+function setCORSHeaders(origin: string | null, env: Env) {
+  const defaultHeaders = {
+    'Access-Control-Allow-Methods': '*',
+    'Access-Control-Allow-Headers': '*',
+  };
+
+  if (origin) {
+    const allowedOrigins = [
+      'https://plan2gather.net',
+      'https://plan2gather.pages.dev',
+    ];
+
+    if (env.stage === 'dev') {
+      allowedOrigins.push('http://localhost:4200', 'http://localhost:4400');
+    }
+
+    if (env.stage === 'beta') {
+      allowedOrigins.push('https://beta.plan2gather.net');
+    }
+
+    if (
+      allowedOrigins.includes(origin) ||
+      (env.stage === 'dev' && origin.endsWith('.plan2gather.pages.dev'))
+    ) {
+      return {
+        ...defaultHeaders,
+        'Access-Control-Allow-Origin': origin,
+      };
+    }
+  }
+
+  // If the origin is not allowed, we return the default headers without the
+  // `Access-Control-Allow-Origin` header, which will cause the browser to
+  // block the request.
+  return {
+    ...defaultHeaders,
+  };
+}
 
 export default {
   async fetch(request: Request, rawEnv: Record<string, unknown>) {
-    if (request.method === 'OPTIONS') {
-      return new Response(null, { headers: CORS_HEADERS });
-    }
     // In actually deployed instances (including `wrangler dev`) Cloudflare includes the CF-Ray header
     // this does not get populated in Miniflare during actual local instances.
     const isDeployed = request.headers.get('CF-Ray') != null;
@@ -28,6 +58,12 @@ export default {
       ...rawEnv,
     });
     const plan2GatherEnv = new Env(cloudflareEnv);
+
+    if (request.method === 'OPTIONS') {
+      return new Response(null, {
+        headers: setCORSHeaders(request.headers.get('origin'), plan2GatherEnv),
+      });
+    }
 
     return fetchRequestHandler({
       endpoint: '',
@@ -45,7 +81,7 @@ export default {
           'Access-Control-Max-Age': '1728000',
           'Content-Encoding': 'gzip',
           Vary: 'Accept-Encoding',
-          ...CORS_HEADERS,
+          ...setCORSHeaders(request.headers.get('origin'), plan2GatherEnv),
         },
       }),
     });
