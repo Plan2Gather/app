49e816bf9270a87b327d9122e523ed4ed7470cf8
Chris <chris2fourlaw@gmail.com>
2023-12-10 10:35:56 -0800

    chore: update deps

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
+  root: __dirname,
+  build: {
+    outDir: '../../dist/packages/frontend',
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
   cacheDir: '../../node_modules/.vite/plan2gather',
 
   server: {
@@ -24,6 +32,11 @@ export default defineConfig({
   // },
 
   test: {
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: '../../coverage/packages/frontend',
+      provider: 'v8',
+    },
     globals: true,
     cache: {
       dir: '../../node_modules/.vitest',
