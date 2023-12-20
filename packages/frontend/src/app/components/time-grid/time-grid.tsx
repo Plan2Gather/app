be74c9253b25ae5e2b6d448456df0509baad3579
spencerperley <63747892+spencerperley@users.noreply.github.com>
2023-12-19 19:44:19 -0800

    feat popover (#87)
    * feat popover
    
    * feat: add popover
    
    * fix: timezone
    
    * fix: better popover handling
    
    ---------
    
    Co-authored-by: Chris <chris2fourlaw@gmail.com>

--- packages/frontend/src/app/components/time-grid/time-grid.tsx
+++ packages/frontend/src/app/components/time-grid/time-grid.tsx
@@ -1,17 +1,22 @@
 import { Fragment, useState } from 'react';
+import Popover from '@mui/material/Popover';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { DateTime } from 'luxon';
+import PossibleTime from '../possible-time/possible-time';
+
+type CellData = {
+  color: string;
+  topBorder: string;
+  names: string[];
+  period: { start: DateTime; end: DateTime };
+};
 
 interface TimeGridProps {
-  data: {
-    color: string;
-    topBorder: string;
-    names: string[];
-    period: { start: DateTime; end: DateTime };
-  }[][];
+  data: CellData[][];
   columnLabels: string[];
   rowLabels: string[];
+  timezone: string;
 }
 
 const cellWidth = 100;
@@ -21,16 +26,31 @@ export default function TimeGrid({
   data,
   columnLabels,
   rowLabels,
+  timezone,
 }: TimeGridProps) {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [clickedCell, setClickedCell] = useState<{
-    rowIndex: number;
-    colIndex: number;
-  } | null>(null);
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+  const [selectedCell, setSelectedCell] = useState<CellData | null>(null);
 
-  const handleCellClick = (rowIndex: number, colIndex: number) => {
-    setClickedCell({ rowIndex, colIndex });
+  const handleClick = (
+    event: React.MouseEvent<HTMLButtonElement>,
+    rowIndex: number,
+    colIndex: number
+  ) => {
+    const selectedCellData = data[rowIndex][colIndex];
+    setSelectedCell(selectedCellData);
+
+    if (selectedCellData.names.length > 0) {
+      setAnchorEl(event.currentTarget);
+    }
   };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
 
   return (
     <Box
@@ -79,11 +99,33 @@ export default function TimeGrid({
                 borderRight: '1px solid black',
               }}
               aria-label={`${columnLabels[colIndex]}, ${rowLabels[rowIndex]}`}
-              onClick={() => handleCellClick(rowIndex, colIndex)}
+              onClick={(event) => handleClick(event, rowIndex, colIndex)}
+              component="button"
             />
           ))}
         </Fragment>
       ))}
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+      >
+        {selectedCell && (
+          <PossibleTime
+            dateRange={{
+              start: selectedCell.period.start.toISO()!,
+              end: selectedCell.period.end.toISO()!,
+            }}
+            users={selectedCell.names}
+            timezone={timezone}
+          />
+        )}
+      </Popover>
     </Box>
   );
 }
