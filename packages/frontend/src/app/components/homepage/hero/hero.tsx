f0cca09dfda6312a98d384f8f1fb1a3282199dd3
cjlawson02 <7269141+cjlawson02@users.noreply.github.com>
2023-12-30 22:53:44 -0800

    feat: pwa support (#124)
    * feat: pwa support
    
    * fix: format
    
    * feat: update primary color, fix headers
    
    ---------
    
    Co-authored-by: Chris <chris2fourlaw@gmail.com>

--- packages/frontend/src/app/components/homepage/hero/hero.tsx
+++ packages/frontend/src/app/components/homepage/hero/hero.tsx
@@ -8,15 +8,15 @@ export default function Hero() {
   return (
     <HeroLayout
       sxBackground={{
-        backgroundImage: 'url(/hero.webp)',
+        backgroundImage: 'url(/images/hero.webp)',
         backgroundColor: '#448ab7',
         backgroundPosition: 'center',
       }}
     >
-      <Typography variant="h1" fontWeight={1} align="center" color="text.primary">
+      <Typography variant="h1" fontWeight={1} align="center" color="white">
         Plan2Gather
       </Typography>
-      <Typography component="h2" variant="h6" align="center" color="text.primary">
+      <Typography component="h2" variant="h6" align="center" color="white">
         Discover the lightning-speed hangout harmonizer
       </Typography>
       <Box display="flex" justifyContent="center" alignItems="center" paddingTop={2}>
