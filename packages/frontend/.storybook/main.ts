3280e9fe89b242a389609468c16418024a4e1213
Chris Lawson <git@chrislawson.dev>
2024-04-13 01:59:10 -0700

    chore: upgrade storybook

--- packages/frontend/.storybook/main.ts
+++ packages/frontend/.storybook/main.ts
@@ -2,7 +2,11 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/app/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@chromatic-com/storybook'
+  ],
 
   framework: {
     name: '@storybook/react-vite',
